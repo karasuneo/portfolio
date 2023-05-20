f98d8f757acbc0efc27a5e540a8576ebfbc3ef20
Yusei Mizutani <yusei4520030401@gmail.com>
2023-05-20 14:46:07 +0900

    tailwind インストール

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const Header: FC = () => {
+  return (
+    <>
+      <button class="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">ボタン</button>
+    </>
+  );
+};
